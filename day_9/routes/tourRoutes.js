0a36abd2ed9c8db7fe9fab754aeff590836f0434
KUNAL SINGH <ks820177@gmail.com>
2023-12-27 20:52:17 +0530

    env variables, middleware and express app structure

---
+++ day_9/routes/tourRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const tourController = require("../controllers/tourController");
+
+const router = express.Router();
+
+//middleware to check if id is present or not
+router.param("id", tourController.checkId);
+
+//middleware to check body
+const checkBody = tourController.checkBody;
+
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(checkBody, tourController.createTour);
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
